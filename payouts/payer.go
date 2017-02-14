835602e7f8b540330476a56ab1c010889fee0169
wariner84 <miscio84@virgilio.it>
2017-02-14 07:14:57 +0100

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -35,12 +35,12 @@ type PayoutsConfig struct {
 
 func (self PayoutsConfig) GasHex() string {
 	x := common.String2Big(self.Gas)
-	return common.BigToHash(x).Hex()
+	return hexutil.EncodeBig(x)
 }
 
 func (self PayoutsConfig) GasPriceHex() string {
 	x := common.String2Big(self.GasPrice)
-	return common.BigToHash(x).Hex()
+	return hexutil.EncodeBig(x)
 }
 
 type PayoutsProcessor struct {
@@ -177,7 +177,7 @@ func (u *PayoutsProcessor) process() {
 			break
 		}
 
-		value := common.BigToHash(amountInWei).Hex()
+		value := hexutil.EncodeBig(amountInWei)
 		txHash, err := u.rpc.SendTransaction(u.config.Address, login, u.config.GasHex(), u.config.GasPriceHex(), value, u.config.AutoGas)
 		if err != nil {
 			log.Printf("Failed to send payment to %s, %v Shannon: %v. Check outgoing tx for %s in block explorer and docs/PAYOUTS.md",
